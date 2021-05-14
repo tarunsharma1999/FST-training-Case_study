d89b7776242ca94db20c20105b4b937ce14b9c90
Tarun Sharma <ts5153033@gmail.com>
2021-05-14 15:04:03 +0530

    Update DB.js

--- js/DB.js
+++ js/DB.js
@@ -9,14 +9,20 @@ if (!dbObj) {
 }
 else {
     dbObj.transaction(function (tx) {
-        tx.executeSql('DROP TABLE item');
-        tx.executeSql('CREATE TABLE IF NOT EXISTS item (id, Name, price, active,DateofLaunch, Category, Delivery)');
         
-        tx.executeSql('insert into item values(1, "Pasta", 99, "Yes", "15/03/2017", "Main Course", "Yes")');
-        tx.executeSql('insert into item values(2, "Burger", 199, "No", "15/04/2017", "Starters", "Yes")');
-        tx.executeSql('insert into item values(3, "Pizza", 299, "Yes", "15/05/2017", "Main Course", "No")');
-        tx.executeSql('insert into item values(4, "Sandwitch", 99, "No", "15/06/2017", "Drinks", "No")');
-        console.log("data inserted");
+        tx.executeSql('CREATE TABLE IF NOT EXISTS item (id, Name, price, active,DateofLaunch, Category, Delivery)');
+	    tx.executeSql('SELECT * FROM item', [], function (tx, results) {
+        // This function is for manual insertion of data by developer. 
+            var len = results.rows.length, i;
+            if(len==0){
+			tx.executeSql('insert into item values(1, "Pasta", 99, "Yes", "15/03/2017", "Main Course", "Yes")');
+      			  tx.executeSql('insert into item values(2, "Burger", 199, "No", "15/04/2017", "Starters", "Yes")');
+       			 tx.executeSql('insert into item values(3, "Pizza", 299, "Yes", "15/05/2017", "Main Course", "No")');
+       			 tx.executeSql('insert into item values(4, "Sandwitch", 99, "No", "15/06/2017", "Drinks", "No")');
+		}
+
+
+        });
     });
 
 }
