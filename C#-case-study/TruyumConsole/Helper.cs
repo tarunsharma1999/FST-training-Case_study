66b76e14ed99b059ed2501c9f2a76d6df2e00f26
Tarun Sharma <ts5153033@gmail.com>
2021-06-02 09:20:02 +0530

    Add files via upload

---
+++ C#-case-study/TruyumConsole/Helper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Com.Cognizant.Truyum.Dao
+{
+    public static class Helper
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["SqlCon"].ConnectionString;
+            }
+        }
+        public static SqlConnection getConnection()
+        {
+            SqlConnection con = new SqlConnection(ConnectionString);
+            return con;
+
+        }
+    }
+}
